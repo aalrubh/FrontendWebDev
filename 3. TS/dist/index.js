0140c8784dfdf22077a657a93a0c9deaf2479862
aalrubh <aalrubh@hotmail.com>
2025-09-06 19:54:34 +0300

    Added assigned code to index.ts

--- 3. TS/dist/index.js
+++ 3. TS/dist/index.js
@@ -1,3 +1,28 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+function calculateArea(width, height) {
+    return width * height;
+}
+class Animal {
+    constructor(name) {
+        this.name = name;
+    }
+    speak() {
+        console.log(this.name, " is speaking");
+    }
+}
+class Cat extends Animal {
+    constructor(name) {
+        //I thought it was this.name . . . (forgot super in java)
+        super(name);
+    }
+    speak() {
+        console.log(this.name, " is speaking");
+    }
+}
+let x;
+x = "String";
+console.log(x);
+x = 2025;
+console.log(x);
 //# sourceMappingURL=index.js.map
---
+++ 3. TS/src/index.ts
@@ -0,0 +1,42 @@
+
+interface Car {
+    make: string;
+    model: string;
+    year: number;
+    isElectric: boolean;
+}
+
+function calculateArea(width: number, height: number) {
+    return width * height;
+}
+
+class Animal {
+    name: string;
+
+    constructor(name: string) {
+        this.name = name;
+    }
+
+    speak(): void {
+        console.log(this.name," is speaking");
+    }
+}
+
+class Cat extends Animal {
+    constructor(name: string) {
+        //I thought it was this.name . . . (forgot super in java)
+        super(name);
+    }
+
+    speak(): void {
+        console.log(this.name," is speaking");
+    }
+}
+
+let x: string | number;
+
+x = "String";
+console.log(x);
+
+x = 2025;
+console.log(x);
